49802f849139a121805e75bd5af923ecc7253766
BFO1715 <>
2023-08-17 18:43:21 +0000

    Added Pages

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 
 # load pages scripts
-from app_pages.page_bp_overview import page_bp_overview_body
-from app_pages.page_data_summary import page_data_summary_body
-from app_pages.page_eda_results import page_eda_results_body
-from app_pages.page_data_preprocessing import page_data_preprocessing_body
+from app_pages.page_jws_requirements import page_jws_requirements_body
+from app_pages.page_data_overview import page_data_overview_body
+from app_pages.page_univariate_analysis import page_univariate_analysis_body
+from app_pages.page_bivariate_analysis import page_bivariate_analysis_body
+from app_pages.page_decision_tree import page_decision_tree_body
+from app_pages.page_random_forest import page_random_forest_body
 from app_pages.page_model_performance import page_model_performance_body
-from app_pages.page_conclusion_recommend import page_conclusion_recommend_body
-from app_pages.page_cluster import page_cluster_body
 
-app = MultiPage(app_name="Churnometer")  # Create an instance of the app
+app = MultiPage(app_name="advertising_analysis")  # Create an instance of the app
 
-# Add your app pages here using .add_page()
-app.add_page("Quick Project Summary", page_summary_body)
-app.add_page("Customer Base Churn Study", page_churned_customer_study_body)
-app.add_page("Prospect Churnometer", page_prospect_body)
-app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
-app.add_page("ML: Prospect Churn", page_predict_churn_body)
-app.add_page("ML: Prospect Tenure", page_predict_tenure_body)
-app.add_page("ML: Cluster Analysis", page_cluster_body)
+# App pages 
+app.add_page("JWS Business Requirement", page_jws_requirements_body)
+app.add_page("Data Overview ", page_data_overview_body)
+app.add_page("Univariate Analysis", page_univariate_analysis_body)
+app.add_page("Bivariate Analysis", page_bivariate_analysis_body)
+app.add_page("Decision Tree", page_decision_tree_body)
+app.add_page("Random Forest", page_random_forest_body)
+app.add_page("Model Performace", page_model_performance_body)
 
 app.run()  # Run the  app
