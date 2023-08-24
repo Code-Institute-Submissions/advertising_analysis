4ba6ba4334f02b4f63a8f5e2bce9a58e88f3eaef
BFO1715 <>
2023-08-24 18:11:40 +0000

    Expanded JWS Bus Req to include approack taken

--- app_pages/page_jws_requirements.py
+++ app_pages/page_jws_requirements.py
@@ -5,14 +5,24 @@ def page_jws_requirements_body():
     st.title("JWS Business Requirement")
 
     st.markdown("## Objective")
-    st.write("JWS is an initial stage startup that offers programs on cutting-edge technologies to students and professionals "
-             "to help them upskill/reskill. With a large number of leads being generated regularly, one of the issues faced "
-             "is to identify which of the leads are more likely to convert so that they can allocate resources accordingly. "
+    st.write("JWS is an initial stage startup that offers programs on "
+             "cutting-edge technologies to students and professionals "
+             "to help them upskill. With a large number of leads being "
+             "generated regularly, one of the issues faced "
+             "is to identify which of the leads are more likely to convert so "
+             "that they can allocate resources accordingly. "
              "The objective is to:")
 
-    st.write("- Analyze and build an ML model to help identify which leads are more likely to convert to paid customers,")
+    st.write("- Analyze and build an ML model to help identify which leads are"
+             "more likely to convert to paid customers")
     st.write("- Find the factors driving the lead conversion process")
     st.write("- Create a profile of the leads which are likely to convert")
+
+    st.write("We will approach this task by looking at: ")
+
+    st.write("- Data Overview")
+    st.write("- EDA Results: Univariate and Bivariate")
+    st.write("- Model Performance: Decision Tree and Random Forest")
 
 
 # Call the function to display the page content
