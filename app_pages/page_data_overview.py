91ae4fd053f3f3727ef6f23f781fc83b7c6e9ea8
BFO1715 <>
2023-08-24 20:46:06 +0000

    Added last 5 rows and statistical summary to data overview

--- app_pages/page_data_overview.py
+++ app_pages/page_data_overview.py
@@ -7,11 +7,38 @@ data = pd.read_csv(
 
 
 def page_data_overview_body():
-    st.title("Data Overview")  # Fixed the string closing quotation mark
+    st.title("Data Overview")
 
-    # Display the first 5 rows of the loaded data
+    st.write("The dataset (15 columns) that was analyzed:")
+
+    st.write("- ID")
+    st.write("- Age")
+    st.write("- Current Occupation")
+    st.write("- First Interaction")
+    st.write("- Profile Completed")
+    st.write("- Website Visits")
+    st.write("- Time Spent on Website")
+    st.write("- Page Views per Visit")
+    st.write("- Last Activity")
+    st.write("- Print Media Type1")
+    st.write("- Print Media Type2")
+    st.write("- Digital Media")
+    st.write("- Educational Channels")
+    st.write("- Referral")
+    st.write("- Status")
+
+    st.write("The dataset contains information on 4,612 records with 0 duplicates.")
+
+    # Display the first and last 5 rows of the loaded data
     st.write("Top 5 rows of the data:")
     st.dataframe(data.head(5))
+
+    st.write("Last 5 rows of the data:")
+    st.dataframe(data.tail(5))
+
+    # Display the statistical summary
+    st.write("The statistical summary of the data is as follows.")
+    st.dataframe(data.describe())
 
 
 # Call the function to display the page
